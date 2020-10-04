d4269d0b09ec54227b577ef690aaa8319c1d789d
angels101 <angelscodex101@gmail.com>
2020-10-05 01:12:00 +0300

    the user details to be displayed under the search form

---
+++ src/app/user-details.ts
@@ -0,0 +1,9 @@
+export class UserDetails {
+
+    public username: string;
+    public url: string;
+    public followers: number;
+    public following: number;
+    public date_created: Date;
+
+}
