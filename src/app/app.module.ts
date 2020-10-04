f40a633f7fffdcfec3ff0be210d5c6869c19cb12
angels101 <angelscodex101@gmail.com>
2020-10-04 23:58:14 +0300

    the app module is a mechanism to group components, directives, pipes and services that are related, in such a way that can be combined with other modules to create an application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
